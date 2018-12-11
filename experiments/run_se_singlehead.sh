e04cc60b1bab2389802d63aecc645841463efd02
Raul Puri <raulp@nvidia.com>
2018-12-11 13:41:58 -0800

    semeval script updates

--- experiments/run_se_singlehead.sh
+++ experiments/run_se_singlehead.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-python experiments/run_clf_single_head.py --text-key title --train data/semeval/train.csv  --val data/semeval/val.csv --test data/semeval/test.csv --process-fn process_tweet
+python experiments/run_clf_single_head.py --text-key Tweet --train data/semeval/train.csv  --val data/semeval/val.csv --test data/semeval/test.csv --process-fn process_tweet
